87b506fc75507f85f0e40fe26c3c6c6751bdff50
artemyuu <tema.693168@gmail.com>
2023-08-01 15:34:34 +0400

    Add: build with d.ts

---
+++ src/models/response-locals.interface.d.ts
@@ -0,0 +1,6 @@
+import { DatabaseResponse } from "./database-response.interface";
+import { ServiceResponse } from "./service-response.interface";
+export interface ResponseLocals<T> {
+    databaseResponse: DatabaseResponse<T>;
+    serviceResponse: ServiceResponse;
+}
