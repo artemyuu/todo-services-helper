5ff1272bc69c7c310c364de9cc3fb4c10273a7f0
artemyuu <tema.693168@gmail.com>
2023-08-03 12:23:52 +0400

    Edit: mark deprecated

--- src/middlewares/send-response.middleware.ts
+++ src/middlewares/send-response.middleware.ts
@@ -1,6 +1,9 @@
 import { Request } from "express";
 import { Response } from "../models";
 
+/**
+ * @deprecated
+ */
 export const sendResponse = (successCode: number = 200, errorCode: number = 400) => (_: Request, res: Response<unknown>) => {
     const statusCode = res.locals.databaseResponse.errors 
         ? errorCode
--- src/models/response-locals.interface.ts
+++ src/models/response-locals.interface.ts
@@ -1,6 +1,9 @@
 import { DatabaseResponse } from "./database-response.interface";
 import { ServiceResponse } from "./service-response.interface";
 
+/**
+ * @deprecated
+ */
 export interface ResponseLocals<T> {
     databaseResponse: DatabaseResponse<T>;
     serviceResponse: ServiceResponse;
